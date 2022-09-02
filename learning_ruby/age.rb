47c489a836b1ace6236573ecf79f13b784b7f276
chr0m0some <leework054@gmail.com>
2022-09-02 15:14:16 -0500

    Starting conditionals

---
+++ learning_ruby/age.rb
@@ -0,0 +1,5 @@
+user_age = gets.chomp
+user_age_to_integer = user_age.to_i
+puts user_age_to_integer + 10
+puts user_age_to_integer + 20
+puts user_age_to_integer + 30
---
+++ learning_ruby/name.rb
@@ -0,0 +1,3 @@
+first_name = gets.chomp
+last_name = gets.chomp
+puts "Greetings #{first_name} #{last_name}"
